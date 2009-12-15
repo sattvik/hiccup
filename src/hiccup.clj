79d4104408a4829406c7e075ecb893c74ba4e4b2
weavejester <jreeves@weavejester.com>
2009-12-15 21:00:36 +0000

    Removed superfluous functions, tidied up code a bit.

--- src/hiccup.clj
+++ src/hiccup.clj
@@ -11,6 +11,10 @@
   (:use clojure.contrib.def)
   (:use clojure.contrib.java-utils))
 
+(defvar *warn-on-interpret* false
+  "Set this binding to true to be warned when code fails to compile and
+   is instead deferred to being interpreted at runtime.")
+
 (defn escape-html
   "Change special characters into HTML character entities."
   [text]
@@ -85,13 +89,10 @@
     (seq? data)    (apply str (map render-html data))
     :otherwise     (as-str data)))
 
-(defvar *warn-on-interpret* true
-  "Set this binding to true to be warned when code fails to compile and
-   is instead deferred to being interpreted at runtime.")
-
 (defvar- steps []
-  "This is a dynamically bound vector that builds up a list of strings and
-   actions, which will then be compiled into operations on a StringBuilder.")
+  "A dynamically bound vector that builds up a list of strings and actions,
+  which will then be compiled into operations on a StringBuilder.")
+
 (defvar- builder-name 'string-builder
   "This will be the name of a lexically scoped variable that builds up
    the result string.")
@@ -113,6 +114,7 @@
       (recur (first rest-items) (rest rest-items)))))
 
 (declare literal?)
+
 (defn- literal-map?
   "Tests whether a map is a literal (ie, all keys and values literal)."
   [m]
@@ -181,56 +183,18 @@
 (defn- perf-warning
   "If *warn-on-interpret* is true, outputs a performance warning."
   [form]
-  (if *warn-on-interpret*
-    (do
-      (println "------------------------------------------------------")
-      (println "Kickup warning - Could not compile the following form:")
-      (println (str form))
-      (println "This form will be interpreted."))))
-
-(defn- format-attr
-  "Turn a name/value pair into an attribute stringp"
-  [name value]
-  (str " " (as-str name) "=\"" (escape-html value) "\""))
+  (when *warn-on-interpret*
+    (println "------------------------------------------------------")
+    (println "Kickup warning - Could not compile the following form:")
+    (prn form)
+    (println "This form will be interpreted.")))
 
 (defn- compile-attrs
   "Turn a map into a string of sorted HTML attributes."
   [attrs]
-  (cond (literal? attrs)
-        (apply str
-               (sort
-                (for [[attr value] attrs]
-                  (cond
-                   (true? value) (format-attr attr attr)
-                   (not value)   ""
-                   :otherwise    (format-attr attr value)))))
-        :otherwise `(#'make-attrs ~attrs))) ;; Make the attrs at runtime.
-
-(defvar- re-tag
-  #"([^\s\.#]+)(?:#([^\s\.#]+))?(?:\.([^\s#]+))?"
-  "Regular expression that parses a CSS-style id and class from a tag name.")
-
-(defvar- container-tags
-  #{"a" "b" "body" "dd" "div" "dl" "dt" "em" "fieldset" "form" "h1" "h2" "h3"
-    "h4" "h5" "h6" "head" "html" "i" "label" "li" "ol" "pre" "script" "span"
-    "strong" "style" "textarea" "ul"}
-  "A list of tags that need an explicit ending tag when rendered.")
-
-(defn- parse-tag-name
-  "Parse the id and classes from a tag name."
-  [tag]
-    (rest (re-matches re-tag (as-str tag))))
-
-(defn- parse-element
-  "Ensure a tag vector is of the form [tag-name attrs content]."
-  [[tag & content]]
-  (let [[tag id class] (parse-tag-name tag)
-        tag-attrs      {:id id
-                        :class (if class (.replace class "." " "))}
-        map-attrs      (first content)]
-    (if (map? map-attrs)
-      [tag (merge tag-attrs map-attrs) (next content)]
-      [tag tag-attrs content])))
+  (if (literal? attrs)
+    (make-attrs attrs)
+    `(#'make-attrs ~attrs)))
 
 (defn- compile-tag-2-ambiguous
   "Compile an HTML tag with 2 elements, of which the second is not a literal,
@@ -262,6 +226,7 @@
                             "</" ~tag ">")))))
 
 (declare compile-html)
+
 (defn- compile-tag
   "Compile an HTML tag represented as a vector to the code to render
    it as a string of HTML."
@@ -273,20 +238,16 @@
         ;; So we can compile it, but it's not all literals, and only 2 elements:
         ;; Special case to handle deferred 2nd element, ambiguous til runtime.
         (if (container-tags tag)
-          (do
-            (emit "<" tag)
-            (compile-tag-2-ambiguous tag attrs content false))
-          (do
-            (emit "<" tag)
-            (compile-tag-2-ambiguous tag attrs content true)))
+          (do (emit "<" tag)
+              (compile-tag-2-ambiguous tag attrs content false))
+          (do (emit "<" tag)
+              (compile-tag-2-ambiguous tag attrs content true)))
         ;; Else, we have a routine that will compile it otherwise.
         (if (or content (container-tags tag))
-          (do
-            (emit "<" tag (compile-attrs attrs) ">")
-            (compile-html content)
-            (emit "</" tag ">"))
-          (do
-            (emit "<" tag (compile-attrs attrs) " />")))))
+          (do (emit "<" tag (compile-attrs attrs) ">")
+              (compile-html content)
+              (emit "</" tag ">"))
+          (do (emit "<" tag (compile-attrs attrs) " />")))))
     (do (perf-warning element)
         (emit `(#'render-tag ~element)))))
 
@@ -295,16 +256,16 @@
    as a string of HTML."
   [data]
   (cond
-   (vector? data) (compile-tag data)
-   (seq? data)    (if (or (vector? (first data))
-                          (string? (first data))
-                          (number? (first data))
-                          (symbol? (first data)))
-                    (apply str (map compile-html data))
-                    (do (perf-warning data)
-                        (emit `(apply str (map #'render-html (vector ~@data))))))
-   (symbol? data) (emit `(apply str ~data))
-   :otherwise     (emit (as-str data))))
+    (vector? data) (compile-tag data)
+    (seq? data)    (if (or (vector? (first data))
+                           (string? (first data))
+                           (number? (first data))
+                           (symbol? (first data)))
+                     (apply str (map compile-html data))
+                     (do (perf-warning data)
+                         (emit `(apply str (map #'render-html (vector ~@data))))))
+    (symbol? data) (emit `(apply str ~data))
+    :otherwise     (emit (as-str data))))
 
 (defmacro html
   "Compile Clojure data structs to fast functions producing a string of HTML."
